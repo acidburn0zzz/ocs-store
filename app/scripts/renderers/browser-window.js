90e9a6801a0f4ad8455cb43c13553096e87e3b1d
Akira Ohgaki <akiraohgaki@gmail.com>
2019-01-21 17:17:42 +0900

    Update browser-window

---
+++ app/scripts/renderers/browser-window.js
@@ -0,0 +1,26 @@
+const {ipcRenderer} = require('electron');
+
+//const packageMeta = require('../../../package.json');
+//const appConfig = require('../../configs/application.json');
+
+import Chirit from '../../libs/chirit/Chirit.js';
+
+import RootComponent from '../components/RootComponent.js';
+import PageComponent from '../components/PageComponent.js';
+
+import OcsManagerWsApi from '../api/OcsManagerWsApi.js';
+
+import OcsManagerModule from '../modules/OcsManagerModule.js';
+
+RootComponent.define('root-component');
+const stateManager = new Chirit.StateManager('root-component');
+stateManager.target.state = stateManager.state;
+
+const ocsManagerUrl = ipcRenderer.sendSync('data', 'ocsManagerUrl');
+const ocsManagerWsApi = new OcsManagerWsApi(ocsManagerUrl);
+
+new OcsManagerModule(stateManager, ocsManagerWsApi);
+
+PageComponent.define('page-component');
+
+//stateManager.dispatch('');
