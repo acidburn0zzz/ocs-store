a88cf2e5394941ac6410172698765b1b5f2eeb00
Akira Ohgaki <akiraohgaki@gmail.com>
2019-01-22 01:46:16 +0900

    Webview

--- app/scripts/components/WebviewComponent.js
+++ app/scripts/components/WebviewComponent.js
@@ -1,3 +1,5 @@
+const ElectronStore = require('electron-store');
+
 import BaseComponent from './BaseComponent.js';
 
 export default class WebviewComponent extends BaseComponent {
@@ -5,9 +7,18 @@ export default class WebviewComponent extends BaseComponent {
     render() {
         return `
             ${this.sharedStyle}
-
-            <webview></webview>
         `;
+    }
+
+    componentUpdatedCallback() {
+        const store = new ElectronStore({name: 'application'});
+
+        const webviewElement = document.createElement('webview');
+        webviewElement.setAttribute('partition', 'persist:opendesktop');
+        webviewElement.setAttribute('preload', './scripts/renderers/webview-ipc.js');
+        webviewElement.setAttribute('src', store.get('startPage'));
+        webviewElement.className = 'flex-auto';
+        this.contentRoot.appendChild(webviewElement);
     }
 
 }
