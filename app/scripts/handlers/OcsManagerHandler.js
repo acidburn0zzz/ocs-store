4286c7fbb6754f3b99f7fc9999ba00ca9dd03d81
Akira Ohgaki <akiraohgaki@gmail.com>
2019-02-09 06:57:18 +0900

    Fix for websocket message receive

--- app/scripts/handlers/OcsManagerHandler.js
+++ app/scripts/handlers/OcsManagerHandler.js
@@ -15,22 +15,7 @@ export default class OcsManagerHandler {
 
         this._subscribe();
 
-        this._ocsManagerApi.callback
-            .set('ItemHandler::metadataSetChanged', this.ItemHandler_metadataSetChanged.bind(this))
-            .set('ItemHandler::downloadStarted', this.ItemHandler_downloadStarted.bind(this))
-            .set('ItemHandler::downloadFinished', this.ItemHandler_downloadFinished.bind(this))
-            .set('ItemHandler::downloadProgress', this.ItemHandler_downloadProgress.bind(this))
-            .set('ItemHandler::saveStarted', this.ItemHandler_saveStarted.bind(this))
-            .set('ItemHandler::saveFinished', this.ItemHandler_saveFinished.bind(this))
-            .set('ItemHandler::installStarted', this.ItemHandler_installStarted.bind(this))
-            .set('ItemHandler::installFinished', this.ItemHandler_installFinished.bind(this))
-            .set('ItemHandler::uninstallStarted', this.ItemHandler_uninstallStarted.bind(this))
-            .set('ItemHandler::uninstallFinished', this.ItemHandler_uninstallFinished.bind(this))
-            .set('UpdateHandler::checkAllStarted', this.UpdateHandler_checkAllStarted.bind(this))
-            .set('UpdateHandler::checkAllFinished', this.UpdateHandler_checkAllFinished.bind(this))
-            .set('UpdateHandler::updateStarted', this.UpdateHandler_updateStarted.bind(this))
-            .set('UpdateHandler::updateFinished', this.UpdateHandler_updateFinished.bind(this))
-            .set('UpdateHandler::updateProgress', this.UpdateHandler_updateProgress.bind(this));
+        this._receive();
     }
 
     _subscribe() {
@@ -86,128 +71,115 @@ export default class OcsManagerHandler {
             });
     }
 
-    //// For ocsManagerApi ////
+    _receive() {
+        this._ocsManagerApi.callback
+            .set('ItemHandler::metadataSetChanged', () => {
+                this._stateManager.dispatch('ocsManager_downloading', {});
+            })
+            .set('ItemHandler::downloadStarted', (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_downloadstart') {
+                    console.error(new Error(message.data[0].message));
+                }
+                else if (message.data[0].metadata.command === 'install') {
+                    // download preview pic
+                }
+                // update component
+            })
+            .set('ItemHandler::downloadFinished', (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_download') {
+                    console.error(new Error(message.data[0].message));
+                }
+                // update component
+            })
+            .set('ItemHandler::downloadProgress', (message) => {
+                console.log(message);
+                // update component
+            })
+            .set('ItemHandler::saveStarted', (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_savestart') {
+                    console.error(new Error(message.data[0].message));
+                }
+                // update component
+            })
+            .set('ItemHandler::saveFinished', (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_save') {
+                    console.error(new Error(message.data[0].message));
+                }
+                // update component
+            })
+            .set('ItemHandler::installStarted', (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_installstart') {
+                    console.error(new Error(message.data[0].message));
+                }
+                // update component
+            })
+            .set('ItemHandler::installFinished', async (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_install') {
+                    console.error(new Error(message.data[0].message));
+                }
 
-    ItemHandler_metadataSetChanged() {
-        this._stateManager.dispatch('ocsManager_downloading', {});
-    }
+                // update component
 
-    ItemHandler_downloadStarted(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_downloadstart') {
-            console.error(new Error(message.data[0].message));
-        }
-        else if (message.data[0].metadata.command === 'install') {
-            // download preview pic
-        }
-        // update component
-    }
+                message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigInstalledItems', []);
+                this._installedItems = message.data[0];
+            })
+            .set('ItemHandler::uninstallStarted', (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_uninstallstart') {
+                    console.error(new Error(message.data[0].message));
+                }
+            })
+            .set('ItemHandler::uninstallFinished', async (message) => {
+                console.log(message);
+                if (message.data[0].status !== 'success_uninstall') {
+                    console.error(new Error(message.data[0].message));
+                }
+                message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigInstalledItems', []);
+                this._installedItems = message.data[0];
+                message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigUpdateAvailableItems', []);
+                this._updateAvailableItems = message.data[0];
 
-    ItemHandler_downloadFinished(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_download') {
-            console.error(new Error(message.data[0].message));
-        }
-        // update component
-    }
-
-    ItemHandler_downloadProgress(message) {
-        console.log(message);
-        // update component
-    }
-
-    ItemHandler_saveStarted(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_savestart') {
-            console.error(new Error(message.data[0].message));
-        }
-        // update component
-    }
-
-    ItemHandler_saveFinished(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_save') {
-            console.error(new Error(message.data[0].message));
-        }
-        // update component
-    }
-
-    ItemHandler_installStarted(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_installstart') {
-            console.error(new Error(message.data[0].message));
-        }
-        // update component
-    }
-
-    async ItemHandler_installFinished(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_install') {
-            console.error(new Error(message.data[0].message));
-        }
-
-        // update component
-
-        message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigInstalledItems', []);
-        this._installedItems = message.data[0];
-    }
-
-    ItemHandler_uninstallStarted(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_uninstallstart') {
-            console.error(new Error(message.data[0].message));
-        }
-    }
-
-    async ItemHandler_uninstallFinished(message) {
-        console.log(message);
-        if (message.data[0].status !== 'success_uninstall') {
-            console.error(new Error(message.data[0].message));
-        }
-        message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigInstalledItems', []);
-        this._installedItems = message.data[0];
-        message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigUpdateAvailableItems', []);
-        this._updateAvailableItems = message.data[0];
-
-        // remove preview pic
-    }
-
-    UpdateHandler_checkAllStarted(message) {
-        console.log(message);
-        if (!message.data[0]) {
-            console.error(new Error('Item update check failed'));
-        }
-    }
-
-    async UpdateHandler_checkAllFinished(message) {
-        console.log(message);
-        if (!message.data[0]) {
-            console.error(new Error('Item update check failed'));
-        }
-        message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigUpdateAvailableItems', []);
-        this._updateAvailableItems = message.data[0];
-        // update component
-    }
-
-    UpdateHandler_updateStarted(message) {
-        console.log(message);
-        if (!message.data[1]) {
-            console.error(new Error('Item update failed'));
-        }
-    }
-
-    async UpdateHandler_updateFinished(message) {
-        console.log(message);
-        if (!message.data[1]) {
-            console.error(new Error('Item update failed'));
-        }
-        message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigUpdateAvailableItems', []);
-        this._updateAvailableItems = message.data[0];
-    }
-
-    UpdateHandler_updateProgress(message) {
-        console.log(message);
-        // update component
+                // remove preview pic
+            })
+            .set('UpdateHandler::checkAllStarted', (message) => {
+                console.log(message);
+                if (!message.data[0]) {
+                    console.error(new Error('Item update check failed'));
+                }
+            })
+            .set('UpdateHandler::checkAllFinished', async (message) => {
+                console.log(message);
+                if (!message.data[0]) {
+                    console.error(new Error('Item update check failed'));
+                }
+                message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigUpdateAvailableItems', []);
+                this._updateAvailableItems = message.data[0];
+                // update component
+            })
+            .set('UpdateHandler::updateStarted', (message) => {
+                console.log(message);
+                if (!message.data[1]) {
+                    console.error(new Error('Item update failed'));
+                }
+            })
+            .set('UpdateHandler::updateFinished', async (message) => {
+                console.log(message);
+                if (!message.data[1]) {
+                    console.error(new Error('Item update failed'));
+                }
+                message = await this._ocsManagerApi.sendSync('ConfigHandler::getUsrConfigUpdateAvailableItems', []);
+                this._updateAvailableItems = message.data[0];
+            })
+            .set('UpdateHandler::updateProgress', (message) => {
+                console.log(message);
+                // update component
+            });
     }
 
 }
