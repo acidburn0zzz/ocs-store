d171b85eb4b633c9f4e75a36113f7aecb370aa56
Akira Ohgaki <akiraohgaki@gmail.com>
2019-02-08 14:28:20 +0900

    Add index.js for components

---
+++ app/scripts/components/index.js
@@ -0,0 +1,38 @@
+import RootComponent from './RootComponent.js';
+RootComponent.define('root-component');
+
+import PageswitcherComponent from './PageswitcherComponent.js';
+PageswitcherComponent.define('pageswitcher-component');
+
+import PageComponent from './PageComponent.js';
+PageComponent.define('page-component');
+
+import ToolbarComponent from './ToolbarComponent.js';
+ToolbarComponent.define('toolbar-component');
+
+import StatusbarComponent from './StatusbarComponent.js';
+StatusbarComponent.define('statusbar-component');
+
+import WebviewComponent from './WebviewComponent.js';
+WebviewComponent.define('webview-component');
+
+import ButtonComponent from './ButtonComponent.js';
+ButtonComponent.define('button-component');
+
+import NavbuttonComponent from './NavbuttonComponent.js';
+NavbuttonComponent.define('navbutton-component');
+
+import MenubuttonComponent from './MenubuttonComponent.js';
+MenubuttonComponent.define('menubutton-component');
+
+import OmniboxComponent from './OmniboxComponent.js';
+OmniboxComponent.define('omnibox-component');
+
+import DialogComponent from './DialogComponent.js';
+DialogComponent.define('dialog-component');
+
+import AboutdialogComponent from './AboutdialogComponent.js';
+AboutdialogComponent.define('aboutdialog-component');
+
+import CollectiondialogComponent from './CollectiondialogComponent.js';
+CollectiondialogComponent.define('collectiondialog-component');
