5536d7a4d3c1647d50d2dc4dbf3e69870f67634e
Akira Ohgaki <akiraohgaki@gmail.com>
2019-03-06 04:59:51 +0900

    Cleanup

--- app/scripts/components/common/BaseComponent.js
+++ app/scripts/components/common/BaseComponent.js
@@ -4,20 +4,6 @@ import Chirit from '../../../libs/chirit/Chirit.js';
 
 export default class BaseComponent extends Chirit.Component {
 
-    constructor() {
-        super();
-        this.html = html;
-    }
-
-    setContent(content) {
-        if (content instanceof TemplateResult) {
-            render(content, this.contentRoot);
-        }
-        else {
-            super.setContent(content);
-        }
-    }
-
     get sharedStyle() {
         return `
             @import url(styles/reset.css);
@@ -32,6 +18,19 @@ export default class BaseComponent extends Chirit.Component {
     convertItemKeyToPreviewpicFilename(itemKey) {
         // See also btoa() in main.js
         return btoa(itemKey).slice(-255);
+    }
+
+    get html() {
+        return html;
+    }
+
+    setContent(content) {
+        if (content instanceof TemplateResult) {
+            render(content, this.contentRoot);
+        }
+        else {
+            super.setContent(content);
+        }
     }
 
 }
