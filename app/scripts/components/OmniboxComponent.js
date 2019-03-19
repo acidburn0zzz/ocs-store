1ffdfe17e3c8b8439f63dbc8190471ce5b83eab4
Akira Ohgaki <akiraohgaki@gmail.com>
2019-03-20 06:09:31 +0900

    Fix color

--- app/scripts/components/OmniboxComponent.js
+++ app/scripts/components/OmniboxComponent.js
@@ -71,7 +71,7 @@ export default class OmniboxComponent extends BaseComponent {
                 background-color: var(--color-active);
             }
             div[data-omnibox][data-update-state="active"] {
-                border-color: var(--color-important);
+                border-color: var(--color-warning);
             }
             div[data-omnibox][data-download-state="active"] {
                 border-color: var(--color-information);
@@ -129,7 +129,7 @@ export default class OmniboxComponent extends BaseComponent {
                 border-bottom: 0;
             }
             div[data-palette] div[data-content][data-update-state] a {
-                color: var(--color-important);
+                color: var(--color-warning);
             }
             div[data-palette] div[data-content][data-update-state="inactive"] {
                 display: none;
