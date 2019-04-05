8e51d539e54d2c909de38903150c7786f5eb4c2b
Akira Ohgaki <akiraohgaki@gmail.com>
2019-04-06 04:20:34 +0900

    Fix smooth scrolling

--- app/scripts/renderers/webview.js
+++ app/scripts/renderers/webview.js
@@ -1,29 +1,55 @@
 const {ipcRenderer} = require('electron');
 
 function applySmoothScroll() {
-    let timeoutId = null;
+    let requestId = null;
+    let frame = 0;
+    const frameMax = 50;
+    let amount = 0;
+    const amountMax = 7;
     let deltaX = 0;
     let deltaY = 0;
-    const amount = 2;
+    let scrollLeft = 0;
+    let scrollTop = 0;
+
+    const easeOut = (p) => {
+        return p * (2 - p);
+    };
+
+    const scroll = () => {
+        frame++;
+        document.scrollingElement.scrollTo({
+            left: scrollLeft + (deltaX * easeOut(frame / frameMax)),
+            top: scrollTop + (deltaY * easeOut(frame / frameMax))
+        });
+        if (frame === frameMax) {
+            frame = 0;
+            amount = 0;
+            cancelAnimationFrame(requestId);
+        }
+        else {
+            requestId = requestAnimationFrame(scroll);
+        }
+    };
 
     document.scrollingElement.addEventListener('wheel', (event) => {
         event.preventDefault();
-
-        deltaX += event.deltaX * amount;
-        deltaY += event.deltaY * amount;
-
-        if (timeoutId === null) {
-            timeoutId = setTimeout(() => {
-                document.scrollingElement.scrollBy({
-                    left: deltaX,
-                    top: deltaY,
-                    behavior: 'smooth'
-                });
-                timeoutId = null;
-                deltaX = 0;
-                deltaY = 0;
-            }, 150);
+        if (Math.sign(deltaX) !== Math.sign(event.deltaX)
+            || Math.sign(deltaY) !== Math.sign(event.deltaY)
+        ) {
+            amount = 0;
         }
+        if (amount < amountMax) {
+            amount++;
+        }
+        deltaX = event.deltaX * amount;
+        deltaY = event.deltaY * amount;
+        scrollLeft = document.scrollingElement.scrollLeft;
+        scrollTop = document.scrollingElement.scrollTop;
+        if (frame) {
+            frame = 0;
+            cancelAnimationFrame(requestId);
+        }
+        scroll();
     });
 }
 
